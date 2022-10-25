9eb78765495d4d1aa756ad259c8be4ed23418c9a
SergioSRS <sergioriverasalgado15@gmail.com>
2022-10-25 23:55:42 +0200

    cambios y version4

---
+++ registroV4/registro4.js
@@ -0,0 +1,113 @@
+window.onload = iniciar;
+//Esto es un array bidimensional
+const provincias = [
+	["Selecciona una comunidad autónoma"],
+	["Almería", "Cádiz", "Córdoba", "Granada", "Huelva", "Jaén", "Málaga", "Sevilla"],
+	["Huesca", "Teruel", "Zaragoza"],
+	["Asturias"],
+	["Balears, Illes"],
+	["Palmas, Las", "Santa Cruz de Tenerife"],
+	["Cantabria"],
+	["Ávila", "Burgos", "León", "Palencia", "Salamanca", "Segovia", "Soria", "Valladolid", "Zamora"],
+	["Albacete", "Ciudad Real", "Cuenca", "Guadalajara", "Toledo"],
+	["Barcelona", "Girona", "Lleida", "Tarragona"],
+	["Alicante / Alacant", "Castellón / Castelló", "Valencia / València"],
+	["Badajoz", "Cáceres"],
+	["Coruña, A", "Lugo", "Ourense", "Pontevedra"],
+	["Madrid"],
+	["Murcia"],
+	["Navarra"],
+	["Araba/Álava", "Gipuzkoa", "Bizkaia"],
+	["Rioja, La"],
+	["Ceuta"],
+	["Melilla"]
+]
+function iniciar(){
+	
+	let imagenxd = document.getElementsByTagName('img')[0];
+		imagenxd.addEventListener('click',pulsar);
+	
+	let aceptarxd = document.getElementsByTagName('button')[0];
+		aceptarxd.addEventListener('click',aceptar);
+	
+	let cancelarxd = document.getElementsByTagName('button')[1];
+		cancelarxd.addEventListener('click',cancelar);
+
+	let seleccionarxd = document.getElementsByTagName('select')[0];
+		seleccionarxd.addEventListener('change',seleccionar)
+	
+	let fechaxd = document.getElementsByTagName('input')[2];
+		fechaxd.addEventListener('change',validarFecha)
+
+	document.getElementsByTagName('select')[1].style.display = "none";
+	document.getElementById("formulario").style.visibility = "hidden";
+	document.getElementsByTagName('img')[1].style.display = "none";
+	
+	let imagen2xd = document.getElementsByTagName('img')[1];
+		imagen2xd.addEventListener('mouseover', cambiarFoto)
+		imagen2xd.addEventListener('mouseout', cambiarFoto2)
+		
+	}
+function pulsar()
+	{
+		document.getElementById("formulario").style.visibility = "visible";
+	}
+function aceptar()
+	{
+		document.getElementById("formulario").style.visibility = "hidden";
+		alert("El usuario ha sido registrado");
+	}
+		
+function cancelar()
+	{
+		document.getElementsByTagName('input')[0].value="";
+		document.getElementsByTagName('input')[1].value="";
+	}
+function seleccionar()
+{	//Con esto estoy sacando el valor que nos introduce el usuario
+	let seleccion = document.getElementsByTagName('select')[0][document.getElementsByTagName('select')[0].selectedIndex].value
+	console.log(seleccion)
+	let num_provincias= provincias[seleccion].length;
+	console.log(num_provincias)
+	document.getElementsByTagName('select')[1].length = num_provincias;
+
+	for (let i=0;i<num_provincias;i++)
+	{
+		document.getElementsByTagName('select')[1][i].text=provincias[seleccion][i];
+	}
+	
+	document.getElementsByTagName('select')[1].style.display = "inline";
+}
+function validarFecha(){
+	let fecha= document.getElementsByTagName('input')[2].value
+
+	let fechaNacimiento = new Date(fecha)
+	
+	let fechaActual = new Date()
+	console.log(fechaNacimiento)
+	console.log(fechaActual)
+	console.log(fechaActual-fechaNacimiento)
+	if (fechaActual-fechaNacimiento < 315603596089)
+	{
+		document.getElementsByTagName('img')[1].style.display = "inline";
+	}
+	else
+	{
+		document.getElementsByTagName('img')[1].style.display = "none";
+	}
+}
+function cambiarFoto()
+{
+	let phineasyfer = "imagenes/fineasfer.jpg"
+	document.getElementsByTagName('img')[1].setAttribute('src',phineasyfer);
+}
+function cambiarFoto2()
+{
+	let perry = "imagenes/imagenPerry.webp"
+	document.getElementsByTagName('img')[1].setAttribute('src',perry);
+}
+
+
+	
+	
+
