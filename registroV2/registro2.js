6ecd35559ed56836ce0e9e3aac48520fca558f37
SergioSRS <sergioriverasalgado15@gmail.com>
2022-10-25 20:10:47 +0200

    version 2 casi terminada

--- registroV2/registro2.js
+++ registroV2/registro2.js
@@ -1,4 +1,27 @@
 window.onload = iniciar;
+//Esto es un array bidimensional
+const provincias = [
+	["Selecciona una comunidad autónoma"],
+	["Almería", "Cádiz", "Córdoba", "Granada", "Huelva", "Jaén", "Málaga", "Sevilla"],
+	["Huesca", "Teruel", "Zaragoza"],
+	["Asturias"],
+	["Balears, Illes"],
+	["Palmas, Las", "Santa Cruz de Tenerife"],
+	["Cantabria"],
+	["Ávila", "Burgos", "León", "Palencia", "Salamanca", "Segovia", "Soria", "Valladolid", "Zamora"],
+	["Albacete", "Ciudad Real", "Cuenca", "Guadalajara", "Toledo"],
+	["Barcelona", "Girona", "Lleida", "Tarragona"],
+	["Alicante / Alacant", "Castellón / Castelló", "Valencia / València"],
+	["Badajoz", "Cáceres"],
+	["Coruña, A", "Lugo", "Ourense", "Pontevedra"],
+	["Madrid"],
+	["Murcia"],
+	["Navarra"],
+	["Araba/Álava", "Gipuzkoa", "Bizkaia"],
+	["Rioja, La"],
+	["Ceuta"],
+	["Melilla"]
+]
 function iniciar(){
 	
 	let imagenxd = document.getElementsByTagName('img')[0];
@@ -9,8 +32,12 @@ function iniciar(){
 	
 	let cancelarxd = document.getElementsByTagName('button')[1];
 		cancelarxd.addEventListener('click',cancelar);
-		
-		document.getElementById("formulario").style.visibility = "hidden";
+
+	let seleccionarxd = document.getElementsByTagName('select')[0];
+		seleccionarxd.addEventListener('change',seleccionar)
+	
+	document.getElementsByTagName('select')[1].style.display = "none";
+	document.getElementById("formulario").style.visibility = "hidden";
 		
 	}
 function pulsar()
@@ -28,4 +55,24 @@ function cancelar()
 		document.getElementsByTagName('input')[0].value="";
 		document.getElementsByTagName('input')[1].value="";
 	}
+function seleccionar()
+
+{	//Con esto estoy sacando el valor que nos introduce el usuario
+	let seleccion = document.getElementsByTagName('select')[0][document.getElementsByTagName('select')[0].selectedIndex].value
+	console.log(seleccion)
+	let num_provincias= provincias[seleccion].length;
+	console.log(num_provincias)
+	document.getElementsByTagName('select')[1].length = num_provincias;
+
+	for (let i=0;i<num_provincias;i++)
+	{
+		document.getElementsByTagName('select')[1][i].text=provincias[seleccion][i];
+	}
+	
+	document.getElementsByTagName('select')[1].style.display = "inline";
+}
+
+
+	
+	
 
